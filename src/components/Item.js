23a55ff9bf7d1492dd17a8cfe0e1a7115672d5e1
Penelope Drago <prd@drago.com.au>
2023-05-17 14:55:41 +1000

    add code to make app dynamic

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+  const buttonText = isInCart ? "Remove From Cart" : "Add to Cart";
+  const appClass = isInCart ? "in-cart" : "";
+
+  function handleClick(){
+    setIsInCart(!isInCart);
+  }
+  
   return (
-    <li className="">
+    <li className={appClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{buttonText}</button>
     </li>
   );
 }
